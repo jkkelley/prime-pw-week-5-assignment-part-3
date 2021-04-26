4feac3c74ff11c24e381468ee01590f101fb13cf
James Kelley <jkkelley86@gmail.com>
2021-04-26 15:01:30 -0500

    added showCollection function, called showCollection to test and it works

--- assignment/scripts/music-collection.js
+++ assignment/scripts/music-collection.js
@@ -6,11 +6,12 @@ function addToCollection (title, artist, yearPublished) {
   let bandName = {
     title: title,
     artist: artist,
-    yearPublished: yearPublished
+    year: yearPublished
   }
   collection.push(bandName);
   return bandName;
 } // end addToCollection
+
 
 console.log(addToCollection('10,000 Days', 'Tool', 2006));
 console.log(addToCollection('Lateralus', 'Tool', 2001));
@@ -20,3 +21,17 @@ console.log(addToCollection('Battles', 'In Flames', 2016));
 console.log(addToCollection('A Sense of Purpose', 'In Flames', 2008));
 
 console.log(collection);
+
+
+function showCollection(array) {
+  console.log(array.length);
+  // Below we need to iterate over the whole array.
+  for (let i = 0; i < array.length; i++) {
+    // Below we're accessing the title, artist, and year each time we iterate through our
+    // loop.
+    console.log(`${array[i].title} by ${array[i].artist}, published in ${array[i].year}.`);
+  } // end of loop
+} // end showCollection
+
+
+console.log(showCollection(collection));
