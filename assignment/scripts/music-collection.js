94ecf18be404b1ef2bac7129033b1c00bc640050
James Kelley <jkkelley86@gmail.com>
2021-05-01 09:39:11 -0500

    updated  addToCollection() v1.2

--- assignment/scripts/music-collection.js
+++ assignment/scripts/music-collection.js
@@ -2,37 +2,182 @@ console.log('***** Music Collection *****')
 
 let collection = [];
 
-function addToCollection (title, artist, yearPublished) {
-  let bandName = {
-    title: title,
-    artist: artist,
-    year: yearPublished
-  }
-  collection.push(bandName);
-  return bandName;
-} // end addToCollection
+// // Below is the first addToCollection() and testing I came up with.
+
+// function addToCollection (title, artist, yearPublished) {
+//   let bandName = {
+//     title: title,
+//     artist: artist,
+//     year: yearPublished,
+//   }
+//   collection.push(bandName);
+//   return bandName;
+// } // end addToCollection
+//
+// // Testing addToCollection()
+// console.log(addToCollection('10,000 Days', 'Tool', 2006));
+// console.log(addToCollection('Lateralus', 'Tool', 2001));
+// console.log(addToCollection('The Slim Shady LP', 'Eminem', 1999));
+// console.log(addToCollection('Electric Tears', 'Buckethead', 2002));
+// console.log(addToCollection('Battles', 'In Flames', 2016));
+// console.log(addToCollection('A Sense of Purpose', 'In Flames', 2008));
+//
+// console.log(collection);
+
+// console.log(collection[0].tracks.[0].name);
+
+// <--- S t r e t c h   G o a l s ---> //
+// Updated addToCollection() version 1.1
+
+// I didn't quite like this way of doing it, so I came up with another way to do it.
+// I'm really looking forward to see how we tackle this in next week's meeting.
+
+// // We added the rest parameter to be able to take in as many tracks as needed.
+// function addToCollection (title, artist, yearPublished, ...tracks) {
+//   let bandName = {
+//     title: title,
+//     artist: artist,
+//     year: yearPublished,
+//     // Using the rest parameter automatically generates an array for us.
+//     tracks: tracks
+//   }
+//   collection.push(bandName);
+//   return bandName;
+// } // end addToCollection
+//
+// // Testing new addToCollection(). I will add the first three songs of each album
+// // for the sake of keep the script small.
+//
+// console.log(addToCollection('10,000 Days', 'Tool', 2006,
+// {name: 'Vicarious', duration: '7:06'},
+// {name: 'Jambi', duration: '7:29'},
+// {name: 'Wings For Marie (Part 1)', duration: '6:11'}));
+//
+// console.log(addToCollection('Lateralus', 'Tool', 2001,
+// {name: 'The Grudge', duration: '8:36'},
+// {name: 'Eon Blue Apocalypse', duration: '1:04'},
+// {name: 'The Patient', duration: '7:14'}));
+//
+// console.log(addToCollection('The Slim Shady LP', 'Eminem', 1999,
+// {name: 'Public Service Announcement', duration: '0:34'},
+// {name: 'My Name Is', duration: '4:28'},
+// {name: 'Guilty Conscience', duration: '3:19'}));
+//
+// console.log(addToCollection('Electric Tears', 'Buckethead', 2002,
+// {name: 'All In The Waiting', duration: '3:44'},
+// {name: 'Sketches of Spain (for Miles)', duration: '4:04'},
+// {name: 'Padmasana', duration: '11:39'}));
+//
+// console.log(addToCollection('Battles', 'In Flames', 2016,
+// {name: 'Drained', duration: '4:07'},
+// {name: 'The End', duration: '3:56'},
+// {name: 'Like Sand', duration: '3:44'}));
+//
+// console.log(addToCollection('A Sense of Purpose', 'In Flames', 2008,
+// {name: 'The Mirror\'s Truth', duration: '2:59'},
+// {name: 'Disconnected', duration: '3:37'},
+// {name: 'Sleepless Again', duration: '4:10'}));
+//
+// console.log(collection);
 
 
-console.log(addToCollection('10,000 Days', 'Tool', 2006));
-console.log(addToCollection('Lateralus', 'Tool', 2001));
-console.log(addToCollection('The Slim Shady LP', 'Eminem', 1999));
-console.log(addToCollection('Electric Tears', 'Buckethead', 2002));
-console.log(addToCollection('Battles', 'In Flames', 2016));
-console.log(addToCollection('A Sense of Purpose', 'In Flames', 2008));
+// Updated addToCollection() version 1.2
 
-console.log(collection);
+// This way keeps our function nice and neat. If we pulled Data from an api and formatted it with json lint,
+// we'd be able to take what data we wanted using this method below.
+
+function addToCollection({title, artist, year, tracks}) {
+  // console.log(title, artist, year, tracks);
+  collection.push({title, artist, year, tracks});
+  return collection;
+}
+
+const tool1 = {
+  title: '10,000 Days',
+  artist: 'Tool',
+  year: 2001,
+  tracks: [
+    {name: 'Vicarious', duration: '7:06'},
+    {name: 'Jambi', duration: '7:29'},
+    {name: 'Wings For Marie (Part 1)', duration: '6:11'}
+  ]
+};
+
+const tool2 = {
+  title: 'Lateralus',
+  artist: 'Tool',
+  year: 2006,
+  tracks: [
+    {name: 'The Grudge', duration: '8:36'},
+    {name: 'Eon Blue Apocalypse', duration: '1:04'},
+    {name: 'The Patient', duration: '7:14'}
+  ]
+};
+
+const eminem = {
+  title: 'The Slim Shady LP',
+  artist: 'Eminem',
+  year: 1999,
+  tracks: [
+    {name: 'Public Service Announcement', duration: '0:34'},
+    {name: 'My Name Is', duration: '4:28'},
+    {name: 'Guilty Conscience', duration: '3:19'}
+  ]
+};
+
+const bucketHead = {
+  title: 'Electric Tears',
+  artist: 'Buckethead',
+  year: 2002,
+  tracks: [
+    {name: 'All In The Waiting', duration: '3:44'},
+    {name: 'Sketches of Spain (for Miles)', duration: '4:04'},
+    {name: 'Padmasana', duration: '11:39'}
+  ]
+};
+
+const inFlames1 = {
+  title: 'Battles',
+  artist: 'In Flames',
+  year: 2016,
+  tracks: [
+    {name: 'Drained', duration: '4:07'},
+    {name: 'The End', duration: '3:56'},
+    {name: 'Like Sand', duration: '3:44'}
+  ]
+};
+
+const inFlames2 = {
+  title: 'A Sense of Purpose',
+  artist: 'In Flames',
+  year: 2008,
+  tracks: [
+    {name: 'The Mirror\'s Truth', duration: '2:59'},
+    {name: 'Disconnected', duration: '3:37'},
+    {name: 'Sleepless Again', duration: '4:10'}
+  ]
+};
+
+console.log(addToCollection(tool1));
+console.log(addToCollection(tool2));
+console.log(addToCollection(eminem));
+console.log(addToCollection(bucketHead));
+console.log(addToCollection(inFlames1));
+console.log(addToCollection(inFlames2));
+
 
 
 function showCollection(array) {
   console.log(`Number of Artist in Array: ${array.length}`);
+
   // Below we need to iterate over the whole array.
   for (let i = 0; i < array.length; i++) {
     // Below we're logging out our message each time we iterate through
     // our loop.
-    console.log(`${array[i].title} by ${array[i].artist}, published in ${array[i].year}.`);
+    console.log(`${array[i].title} by ${array[i].artist}, published in ${array[i].year}:`);
+
   } // end of loop
 } // end showCollection
-
 
 console.log(showCollection(collection));
 
@@ -171,7 +316,7 @@ console.log('Expected return - Both Tool Albums:', search({artist: 'Tool'})); //
 console.log('Expected return - {title: "The Slim Shady LP", artist: "Eminem", year: 1999}:', search({year: 1999})); // year
 console.log('Expected return - {title: "A Sense of Purpose", artist: "In Flames", year: 2008}:', search({title: 'A Sense of Purpose', artist: 'In Flames'})); // title and artist
 console.log('Expected return - {title: "Electric Tears", artist: "Buckethead", year: 2002}:', search({title: 'Electric Tears',  year: 2002})); // title and year
-console.log('Expected return - {title: "Lateralus", artist: "Tool", year: 2001}:', search({year: 2001, artist: 'Tool'})); // year and artist
+console.log('Expected return - {title: "Lateralus", artist: "Tool", year: 2006}:', search({year: 2006, artist: 'Tool'})); // year and artist
 console.log('Expected return - {title: "Battles", artist: "In Flames", year: 2016}:', search({title: 'Battles', artist: 'In Flames', year: 2016})); // title, artist, and year
 
 // Testing search() with wrong inputs
